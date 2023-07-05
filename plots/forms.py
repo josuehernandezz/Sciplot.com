8b2c5723f4118bd1a10e3c36a4e329f817cb127f
Josue Hernandez <josueother@gmail.com>
2023-07-05 12:48:22 -0500

    updated form fields

--- plots/forms.py
+++ plots/forms.py
@@ -2,25 +2,71 @@ from django import forms
 from multiupload.fields import MultiFileField
 
 class FileUploadForm(forms.Form):
-    files = MultiFileField(min_num=1, max_num=10)
-    legend_labels = forms.CharField(widget=forms.TextInput(attrs={'class': 'legend-input'})) 
-    title = forms.CharField(widget=forms.TextInput(attrs={'class': 'title-input'}))
-    # x_label = forms.CharField(widget=forms.TextInput(attrs={'class': 'xlabel-input'}))
-    # y_label = forms.CharField(widget=forms.TextInput(attrs={'class': 'ylabel-input'}))
-    # x_axis_min = forms.FloatField(label='X-Axis Min')
-    # x_axis_max = forms.FloatField(label='X-Axis Max')
-    # y_axis_min = forms.FloatField(label='Y-Axis Min')
-    # y_axis_max = forms.FloatField(label='Y-Axis Max')
+    files = MultiFileField(min_num=1, max_num=10,
+                        label="Text Files")
+    
+    legend_labels = forms.CharField(
+                        widget=forms.TextInput(
+                            attrs={
+                                'class': 'legend-input',
+                                'placeholder': 'Label 1, Label 2, Label 3 ...'
+                                }),
+                        label="Legend Labels",
+                        required=False
+                        ) 
+
+    title = forms.CharField(
+                        widget=forms.TextInput(
+                            attrs={
+                                'class': 'title-input',
+                                'placeholder': 'Absorbance & Photoluminescence'
+                                   }),
+                        label='Figure Title',
+                        required=False,
+                        help_text=''
+                        )
 
 class XrdFileUploadForm(forms.Form):
-    cardFiles = MultiFileField(min_num=1, max_num=10)
-    files = MultiFileField(min_num=1, max_num=10)
-    cardfile_labels = forms.CharField(widget=forms.TextInput(attrs={'class': 'cardfile_labels-input'})) 
-    legend_labels = forms.CharField(widget=forms.TextInput(attrs={'class': 'legend-input'})) 
-    title = forms.CharField(widget=forms.TextInput(attrs={'class': 'title-input'}))
-    # x_label = forms.CharField(widget=forms.TextInput(attrs={'class': 'xlabel-input'}))
-    # y_label = forms.CharField(widget=forms.TextInput(attrs={'class': 'ylabel-input'}))
-    # x_axis_min = forms.FloatField(label='X-Axis Min')
-    # x_axis_max = forms.FloatField(label='X-Axis Max')
-    # y_axis_min = forms.FloatField(label='Y-Axis Min')
-    # y_axis_max = forms.FloatField(label='Y-Axis Max')
+    
+    cardFiles = MultiFileField(min_num=1, max_num=10,
+                        label="Card Files")
+    
+    files = MultiFileField(min_num=1, max_num=10,
+                        label="Text Files")
+    
+    cardfile_labels = forms.CharField(
+                    widget=forms.TextInput(attrs={'class': 'cardfile_labels-input'}),
+                    label="Card File Labels"
+                    ) 
+
+    cardfile_labels = forms.CharField(
+                        widget=forms.TextInput(
+                            attrs={
+                                'class': 'cardfile_labels-input',
+                                'placeholder': 'Label 1, Label 2, Label 3 ...'
+                                }),
+                        label="Card File Labels",
+                        required=False
+                        ) 
+
+    legend_labels = forms.CharField(
+                        widget=forms.TextInput(
+                            attrs={
+                                'class': 'legend-input',
+                                'placeholder': 'Label 1, Label 2, Label 3 ...'
+                                }),
+                        label="Legend Labels",
+                        required=False
+                        ) 
+
+    title = forms.CharField(
+                        widget=forms.TextInput(
+                            attrs={
+                                'class': 'title-input',
+                                'placeholder': 'Powder XRD'
+                                   }),
+                        label='Figure Title',
+                        required=False,
+                        help_text=''
+                        )
+
--- plots/views.py
+++ plots/views.py
@@ -6,27 +6,6 @@ from .exampleplots import exampleplot
 
 # Create your views here.
 
-def update_title(request):
-    if request.method == 'POST':
-        new_title = request.POST.get('title')
-        # Update the title as desired
-        print('success ' + new_title)
-
-        form = FileUploadForm(request.POST, request.FILES)
-        files = request.FILES.getlist('files')
-        legend_labels = form.cleaned_data.get('legend_labels').split(',')
-        title = form.cleaned_data.get('title')
-        x_label = 'Wavelength (nm)'
-        y_label = 'Intensity (a.u.)'
-
-        script, div = abspl_plotter(files, legend_labels, title, x_label, y_label)
-        # Redirect to a success page or render a success message
-        return render(request, 'plot.html', {'script': script, 'div': div, 'title': title, 'x_label': x_label, 'y_label': y_label})
-    else:
-        print('Failure')
-        # Return an error response if the request method is not POST
-        return JsonResponse({'success': False, 'message': 'Invalid request method'})
-
 def home(request):
     files = ['static/files/Absorbance.txt','static/files/Photoluminesence.txt']
     legend_labels = ['Abs','PL']
@@ -36,21 +15,20 @@ def home(request):
     script, div = exampleplot(files, legend_labels, title, x_label, y_label)
     return render(request, 'home.html', {'script': script, 'div': div})
 
-def your_view(request):
-    link = "/xrd"  # Replace with the actual link value
-    context = {'link': link}
-    return render(request, 'your_template.html', context)
-
 def abspl(request):
     if request.method == 'POST':
         form = FileUploadForm(request.POST, request.FILES)
         if form.is_valid():
             files = request.FILES.getlist('files')
-            legend_labels = form.cleaned_data.get('legend_labels').split(',')
-            title = form.cleaned_data.get('title')
+            
+            labels = ['Sample ' + str(i) for i in range(len(files))]
+            input_labels = form.cleaned_data.get('legend_labels')
+            legend_labels = input_labels.split(',') if input_labels else labels
+
+            title = form.cleaned_data.get('title') or 'Absorbance & Photoluminescence'
             x_label = 'Wavelength (nm)'
             y_label = 'Intensity (a.u.)'
-            
+
             script, div = abspl_plotter(files, legend_labels, title, x_label, y_label)
             # Redirect to a success page or render a success message
             return render(request, 'plot.html', {'script': script, 'div': div, 'title': title, 'x_label': x_label, 'y_label': y_label})
@@ -65,20 +43,23 @@ def xrd(request):
         if form.is_valid():
             cardFiles = request.FILES.getlist('cardFiles')
             files = request.FILES.getlist('files')
-            cardfile_labels = form.cleaned_data.get('cardfile_labels').split(',')
-            legend_labels = form.cleaned_data.get('legend_labels').split(',')
-            title = form.cleaned_data.get('title')
-            # x_label = '2'r'$\theta$ (degree)'
+            
+            cardlabels = ['Card File ' + str(i) for i in range(len(files))]
+            card_input_labels = form.cleaned_data.get('legend_labels')
+            card_legend_labels = card_input_labels.split(',') if card_input_labels else cardlabels
+
+            labels = ['File ' + str(i) for i in range(len(files))]
+            input_labels = form.cleaned_data.get('legend_labels')
+            legend_labels = input_labels.split(',') if input_labels else labels
+ 
+            title = form.cleaned_data.get('title') or 'Powder XRD'
             x_label = r'2θ (degree)'
             y_label = 'Intensity (a.u.)'
 
-            script, div = xrd_plotter(cardFiles, files, cardfile_labels, legend_labels, title, x_label, y_label)
+            script, div = xrd_plotter(cardFiles, files, card_legend_labels, legend_labels, title, x_label, y_label)
             # Redirect to a success page or render a success message
             return render(request, 'plot.html', {'script': script, 'div': div, 'title': title, 'x_label': x_label, 'y_label': y_label})
     else:
         form = XrdFileUploadForm()
         plot_type = '/pxrd'
     return render(request, 'upload.html', {'form': form, 'plot_type': plot_type})
-
-def plot(request):
-    return render(request, 'plot.html')
