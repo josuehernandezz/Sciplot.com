ecb8bfd60a3f2fef772624622bec1dbbb2328783
Josue Hernandez <josueother@gmail.com>
2024-03-22 14:21:48 -0700

    added tailwindcss and flowbite features

--- src/plots/urls.py
+++ src/plots/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('index', views.index, name='index'),
     path('abspl', views.abspl, name='abspl'),
     path('pxrd', views.xrd, name='xrd'),
     path('ftir', views.ftir, name='ftir'),
     path('plqy', views.plqy, name='plqy'),
     path('universal', views.universal, name='universal'),
+    path('faq', views.faq, name='faq')
 ]
--- src/static/js/theme_detector.js
+++ src/static/js/theme_detector.js
@@ -1,25 +1,6 @@
-// Function to detect color scheme
-function detectColorScheme() {
-    return window.matchMedia('(prefers-color-scheme: dark)').matches;
-  }
-  
-  // Function to send color scheme to the backend
-  function sendColorSchemeToBackend(isDarkMode) {
-    // Create an HTTP request
-    var xhr = new XMLHttpRequest();
-    
-    // Prepare the request
-    xhr.open('POST', '/set-color-scheme', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-  
-    // Prepare the data to be sent
-    var data = JSON.stringify({ isDarkMode: isDarkMode });
-  
-    // Send the request
-    xhr.send(data);
-  }
-  
-  // Detect color scheme and send it to the backend
-  var isDarkMode = detectColorScheme();
-  sendColorSchemeToBackend(isDarkMode);
-  
+  // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+  if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark');
+} else {
+    document.documentElement.classList.remove('dark')
+}
