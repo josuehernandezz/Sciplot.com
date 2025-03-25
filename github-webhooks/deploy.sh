b144ffc81faccd35747fc39b5f6a2098650201e8
Josue Hernandez <josueother@gmail.com>
2025-03-25 15:35:30 -0700

    webhook code test

--- github-webhooks/deploy.sh
+++ github-webhooks/deploy.sh
@@ -11,13 +11,13 @@ docker build -t sciplot . || { echo "Docker build failed"; exit 1; }
 # Check if a container named 'sciplot' is already running
 existing_container=$(docker ps -q -f name=sciplot)
 
-if [[ -n "$existing_container" ]]; then
+if [[ -z "$existing_container" ]]; then
+    echo "Starting new container..."
+    docker run --env-file /home/josue/sciplot/.env -d --name sciplot -p 8000:8000 sciplot || { echo "Docker run failed"; exit 1; }
+else
     echo "Stopping and removing existing container named 'sciplot'..."
     docker stop sciplot || { echo "Failed to stop existing container"; exit 1; }
     docker rm sciplot || { echo "Failed to remove existing container"; exit 1; }
-else
-    echo "Starting new container..."
-    docker run --env-file /home/josue/sciplot/.env -d --name sciplot -p 8000:8000 sciplot || { echo "Docker run failed"; exit 1; }
 fi
 
 echo "Deployment successful! The new container is now running."
