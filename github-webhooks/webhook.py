37e19f52b9b82dc443346ad415ed94a490d01ccc
Josue Hernandez <josueother@gmail.com>
2025-03-25 14:28:11 -0700

    webhook code test

--- github-webhooks/webhook.py
+++ github-webhooks/webhook.py
@@ -61,6 +61,7 @@ def webhook():
         except ValueError as e:
             return jsonify({"error": str(e)}), 403
 
+
         if payload.get('ref') == 'refs/heads/main' and payload.get('repository', {}).get('name') == 'sciplot':  
             # Ensure it's the 'main' branch and the correct repository
             subprocess.run(["/home/josue/github-webhooks/deploy.sh"], check=True)
