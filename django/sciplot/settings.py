0f3fef20df272ef5a900bc35a2265728fcf06779
Josue Hernandez <josueother@gmail.com>
2025-03-25 17:09:06 -0700

    webhook code test

--- django/sciplot/settings.py
+++ django/sciplot/settings.py
@@ -134,3 +134,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://sciplot.heusoj.com']
